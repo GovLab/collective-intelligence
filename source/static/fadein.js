3cde3543c912313e3c2682ef5825fa38973aa5b7
Ena Bek <enabek@gmail.com>
2016-10-17 13:11:44 -0400

    Add fading banner in header

---
+++ source/static/fadein.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  var target = $('.collective-banner').hide(),
+      items = $('.collective-banner').children(),
+      counter = 0;
+  function bannerFade() {
+      target.fadeIn(2000).delay( 2500 ).fadeOut(2000,function() {
+          bannerFade();
+      }).html(items[counter++]);
+      if (counter == items.length) {
+          counter = 0;
+      }
+  }
+  bannerFade();
+});
