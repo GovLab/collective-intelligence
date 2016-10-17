bcfea09b51ce5e016e68b459f6a70873240affbb
mocxd <mark.adkins.hastings@gmail.com>
2016-10-17 17:11:42 -0400

    fix for auto date select feature

--- source/static/manageTimelineSelection.js
+++ source/static/manageTimelineSelection.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
-  var currentDate = new Date($.now()).toISOString().slice(0, 10);
-  var allEvents = $(".e-event").find("time");
+    var allEvents = $(".e-event");
+    var allTimeboxes = $(".e-timebox");
+    var first = true;
 
-  allEvents.each(function() {
-    var eventDate = $(this).attr("datetime");
 
-    if ( eventDate >= currentDate ) {
-      $(".e-event").addClass("m-current");
-    };
+    allEvents.each(function(i) {
+        var eventDate = new Date($(this).find("time").attr("datetime"));
 
-  });
+        if ( first && eventDate >= $.now() ) {
+            $(this).addClass("m-current");
+            $(allTimeboxes.get(i)).addClass("m-current");
+            first = false;
+        };
+
+    });
 
 });
