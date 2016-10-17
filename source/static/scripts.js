893b05d166d87ba115ef6db120b04199d00873ac
Ena Bek <enabek@gmail.com>
2016-10-17 16:39:20 -0400

    Move fadeIn to scripts file

--- source/static/scripts.js
+++ source/static/scripts.js
@@ -73,4 +73,21 @@ $(document).ready(function () {
             }
         ]
     });
+
+    //FadeIn banner
+    $(document).ready(function() {
+      var target = $('.collective-banner').hide(),
+          items = $('.collective-banner').children(),
+          counter = 0;
+      function bannerFade() {
+          target.fadeIn(2000).delay( 2500 ).fadeOut(2000,function() {
+              bannerFade();
+          }).html(items[counter++]);
+          if (counter == items.length) {
+              counter = 0;
+          }
+      }
+      bannerFade();
+    });
+
 }); // doc.ready
