0c9adbc968608294fbebb950d561343ccd52bf6f
Ena Bek <enabek@gmail.com>
2016-11-07 11:35:25 -0500

    Move vendor files into new scripts folder and change path in base.html and change gulp vendor task

---
+++ source/static/scripts/manageTimelineSelection.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    var allEvents = $(".e-event");
+    var allTimeboxes = $(".e-timebox");
+    var mobileTimeboxes = $(".b-timeline-list");
+    var first = true;
+
+    allEvents.each(function(i) {
+        var eventDate = new Date($(this).find("time").attr("datetime"));
+
+        if ( first && eventDate <= $.now() || first && eventDate === $.now()) {
+            $(this).addClass("m-current");
+            $(allTimeboxes.get(i)).addClass("m-current");
+            $(mobileTimeboxes.children().get(i)).addClass("m-current");
+            first = false;
+        };
+    });
+});
