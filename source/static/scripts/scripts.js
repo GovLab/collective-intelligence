38d9899fae2ab99986e4067626e6a5557327a4ac
annabialas <ab5086@nyu.edu>
2016-12-05 11:19:50 -0500

    fixes things?

--- source/static/scripts/scripts.js
+++ source/static/scripts/scripts.js
@@ -75,19 +75,20 @@ $(document).ready(function () {
     // });
 
     //FadeIn banner
-    var target = $('.collective-banner').hide(),
-        items = $('.collective-banner').children(),
-        counter = 0;
-
-    function bannerFade() {
-      target.fadeIn(2000).delay( 2500 ).fadeOut(2000,function() {
-          bannerFade();
-      }).html(items[counter++]);
-      if (counter == items.length) {
+    // $(document).ready(function() {
+      var target = $('.collective-banner').hide(),
+          items = $('.collective-banner').children(),
           counter = 0;
+      function bannerFade() {
+          target.fadeIn(2000).delay( 2500 ).fadeOut(2000,function() {
+              bannerFade();
+          }).html(items[counter++]);
+          if (counter == items.length) {
+              counter = 0;
+          }
       }
-    }
-
-    bannerFade();
+      bannerFade();
+    // });
 
 }); // doc.ready
+
