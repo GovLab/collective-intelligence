2d66751437540f82aeeead10f25f81abc4f29b8f
annabialas <ab5086@nyu.edu>
2016-12-05 11:31:32 -0500

    more hacks

--- source/static/scripts/scripts.js
+++ source/static/scripts/scripts.js
@@ -1,24 +1,24 @@
 $(document).ready(function () {
     // Smooth Scrolling Function
-    $('a[href*=#]:not([href=#])').click(function () {
-        var $targ = $(this.hash),
-            host1 = this.hostname,
-            host2 = location.hostname,
-            path1 = this.pathname.replace(/^\//, ''),
-            path2 = location.pathname.replace(/^\//, '');
+    // $('a[href*=#]:not([href=#])').click(function () {
+    //     var $targ = $(this.hash),
+    //         host1 = this.hostname,
+    //         host2 = location.hostname,
+    //         path1 = this.pathname.replace(/^\//, ''),
+    //         path2 = location.pathname.replace(/^\//, '');
 
-        if (!$targ.length) {
-            $targ = $('[name=' + this.hash.slice(1) + ']');
-        }
+    //     if (!$targ.length) {
+    //         $targ = $('[name=' + this.hash.slice(1) + ']');
+    //     }
 
-        if ($targ.length && (host1 === host2 || path1 === path2)) {
-            $('html, body').animate({ scrollTop: $targ.offset().top }, 1000);
+    //     if ($targ.length && (host1 === host2 || path1 === path2)) {
+    //         $('html, body').animate({ scrollTop: $targ.offset().top }, 1000);
 
-            return false;
-        }
+    //         return false;
+    //     }
 
-        return true;
-    });
+    //     return true;
+    // });
 
     // Modal Click Behavior
     $('.js-open-modal').click(function () {
