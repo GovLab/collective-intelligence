a2d940014de388fcedd5443908aa94012831ab59
enabek <enabek@gmail.com>
2017-03-10 12:33:43 -0500

    Update styleguide and scripts and add list.js cdn

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -21,17 +21,54 @@ $(document).ready(function () {
     });
 
     // Modal Click Behavior
+    // $('.js-open-modal').click(function () {
+    //     $('.js-target-modal').addClass('js-active');
+    //     $('#overlay').addClass('js-active');
+    //     $('body').addClass('js-body-modal-active');
+    // });
+    //
+    // $('.js-close-modal').click(function () {
+    //     $('.js-target-modal').removeClass('js-active');
+    //     $('#overlay').removeClass('js-active');
+    //     $('body').removeClass('js-body-modal-active');
+    // });
+
+    var currentScroll = 0;
+
+    function lockscroll(){
+      $(window).scrollTop(currentScroll);
+    }
+    // Modal Click Behavior
     $('.js-open-modal').click(function () {
-        $('.js-target-modal').addClass('js-active');
+        $(this).parent().find('.js-target-modal').addClass('js-active');
+        currentScroll=$(window).scrollTop();
+        $(window).bind('scroll',lockscroll);
         $('#overlay').addClass('js-active');
         $('body').addClass('js-body-modal-active');
     });
 
-    $('.js-close-modal').click(function () {
+    // Prevent default on footer close
+    $('footer .js-close-modal').on('click', function(e) {
+      e.preventDefault();
+    });
+
+    $('body').on("click", '.js-close-modal', function () {
+        currentScroll=$(window).scrollTop();
+        $(window).unbind('scroll');
         $('.js-target-modal').removeClass('js-active');
         $('#overlay').removeClass('js-active');
         $('body').removeClass('js-body-modal-active');
     });
+
+    // General Click Behavior for Overlay
+    $('#overlay').click(function () {
+        $('.js-active').removeClass('js-active');
+        currentScroll=$(window).scrollTop();
+        $(window).unbind('scroll');
+        $('.js-active-menu').removeClass('js-active-menu');
+        $('body').removeClass('js-body-modal-active');
+    });
+
 
     // Sticky Click Behavior
     $('.js-close-sticky').click(function () {
@@ -60,6 +97,7 @@ $(document).ready(function () {
     // Slider
     $('.slider').slick({
         arrows: true,
+        adaptiveHeight: true,
         draggable: false,
         swipeToSlide: true,
         autoplay: true,
@@ -73,4 +111,5 @@ $(document).ready(function () {
             }
         ]
     });
+
 }); // doc.ready
