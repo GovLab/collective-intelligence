937b534c519898f12a3fd7eb8f33c0bb7ffa3077
Denny Tek <tek.denny@gmail.com>
2017-03-15 21:07:34 -0400

    Add expand/collapse on program sessions

--- source/js/program.js
+++ source/js/program.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
   var closed = true;
   $('.js-open-details').click(function() {
-      if (closed) {
-          $(this).addClass('js-active')
-           $(this).find(".program-table__item--details").addClass("js-active")
-          closed = false;
-      }
-      else {
-          $(this).removeClass('js-active')
-          $(this).find(".program-table__item--details").removeClass("js-active")
-          closed = true;        
-      }
+    if (closed) {
+        $(this).addClass('js-active')
+         $(this).find(".program-table__item--details").addClass("js-active")
+        closed = false;
+    }
+    else {
+        $(this).removeClass('js-active')
+        $(this).find(".program-table__item--details").removeClass("js-active")
+        closed = true;        
+    }
   });
 
 });
