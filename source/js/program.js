4ed319d02e52c5c2f903a4c7ed92d8bbf4626290
Denny Tek <tek.denny@gmail.com>
2017-03-15 20:18:34 -0400

    Add single and multi session panels

---
+++ source/js/program.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  var closed = true;
+  $('.js-open-details').click(function() {
+      if (closed) {
+          $(this).addClass('js-active')
+           $(this).find(".program-table__item--details").addClass("js-active")
+          closed = false;
+      }
+      else {
+          $(this).removeClass('js-active')
+          $(this).find(".program-table__item--details").removeClass("js-active")
+          closed = true;        
+      }
+  });
+
+});
+
