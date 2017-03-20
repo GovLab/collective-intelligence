9fe32a0bf30388aab270cc1d151cf8a018856234
Denny Tek <tek.denny@gmail.com>
2017-03-20 14:00:24 -0400

    Disable pdf creation

--- source/js/pdf.js
+++ source/js/pdf.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-  var doc = new jsPDF({format: 'letter'});
+//   var doc = new jsPDF({format: 'letter'});
 
-  doc.save('collective-intelligence-program-2017.pdf')
+//   doc.save('collective-intelligence-program-2017.pdf')
 
 
-});
+// });
