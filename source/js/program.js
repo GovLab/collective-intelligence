a2e2871b663fb04b898e127792c29154cafa1899
enabek <enabek@gmail.com>
2017-03-27 16:18:52 -0400

    Fix js to close open table item on first click

--- source/js/program.js
+++ source/js/program.js
@@ -1,17 +1,22 @@
 $(document).ready(function(){
   var closed = true;
+  var open = false;
   $('.js-open-details').click(function() {
-    if (closed) {
-        $(this).addClass('js-active')
-         $(this).find(".program-table__item--details").addClass("js-active")
-        closed = false;
+    if ($(this).find(".program-table__item--details").hasClass("js-active")) {
+      open = true;
+      closed = false;
     }
-    else {
-        $(this).removeClass('js-active')
-        $(this).find(".program-table__item--details").removeClass("js-active")
-        closed = true;        
+    if (closed) {
+      $(this).addClass('js-active')
+      $(this).find(".program-table__item--details").addClass("js-active")
+      closed = false;
+    } else if (!closed && open) {
+      $(this).find(".program-table__item--details").removeClass("js-active")
+      closed = true;
+    } else {
+      $(this).removeClass('js-active')
+      $(this).find(".program-table__item--details").removeClass("js-active")
+      closed = true;
     }
   });
-
 });
-
